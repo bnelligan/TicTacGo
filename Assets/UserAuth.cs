b4e83f70e0929f9e703c24b9edd37e1fff3b45fa
Brendan Nelligan <nelligan81@gmail.com>
2019-03-03 16:59:25 -0500

    Fixed android build

--- Assets/UserAuth.cs
+++ Assets/UserAuth.cs
@@ -4,31 +4,31 @@ using UnityEngine;
 
 public class UserAuth : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Awake()
-    {
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
-            var dependencyStatus = task.Result;
-            if (dependencyStatus == Firebase.DependencyStatus.Available)
-            {
-                // Create and hold a reference to your FirebaseApp,
-                // where app is a Firebase.FirebaseApp property of your application class.
-                //   app = Firebase.FirebaseApp.DefaultInstance;
+    //// Start is called before the first frame update
+    //void Awake()
+    //{
+    //    Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+    //        var dependencyStatus = task.Result;
+    //        if (dependencyStatus == Firebase.DependencyStatus.Available)
+    //        {
+    //            // Create and hold a reference to your FirebaseApp,
+    //            // where app is a Firebase.FirebaseApp property of your application class.
+    //            //   app = Firebase.FirebaseApp.DefaultInstance;
 
-                // Set a flag here to indicate whether Firebase is ready to use by your app.
-            }
-            else
-            {
-                UnityEngine.Debug.LogError(System.String.Format(
-                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
-                // Firebase Unity SDK is not safe to use here.
-            }
-        });
-    }
+    //            // Set a flag here to indicate whether Firebase is ready to use by your app.
+    //        }
+    //        else
+    //        {
+    //            UnityEngine.Debug.LogError(System.String.Format(
+    //              "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+    //            // Firebase Unity SDK is not safe to use here.
+    //        }
+    //    });
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 }
